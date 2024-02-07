4c276dcda95602c10601c8249faa4f02e8e3c452
Khromage <94722022+Khromage@users.noreply.github.com>
2024-02-06 18:16:16 -0800

    Basic Stuff

---
+++ My project/Assets/Scripts/Enemy.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    float MaxHealth, CurrentHealth;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        MaxHealth = 10;
+        CurrentHealth = MaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage(float damageAmount)
+    {
+        CurrentHealth = CurrentHealth - damageAmount;
+    }
+}
